4ac5cf7f773143465d4a07370e472e0f607b0bd8
congtien2109 <vancongtien2000@gmail.com>
2022-09-28 23:41:44 +0700

    bt react form

---
+++ src/components/BTForm/FormDangKy.jsx
@@ -0,0 +1,265 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class FormDangKy extends Component {
+  state = {
+    values: {
+      userName: "",
+      fullName: "",
+      phoneNumber: "",
+      email: "",
+    },
+    errors: {},
+    input: "",
+  };
+
+  handleState = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      // [event.target.name]: event.target.value
+      values: {
+        ...this.state.values,
+        [name]: value,
+      },
+    });
+    console.log(event.target.value);
+    // console.log(event.target.name)
+    // console.log()
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+
+    // chặn event không thêm được khi click đăng ký
+    if (!event.target.checkValidity()) {
+      return;
+    }
+
+    this.props.dispatch({
+      type: this.props.selectedUser ? "UPDATE_USER" : "ADD_USER",
+      payload: this.state.values,
+    });
+
+    this.setState({
+      values: {
+        userName: "",
+        fullName: "",
+        phoneNumber: "",
+        email: "",
+      },
+      input: "",
+    });
+  };
+  handleInput = (event) => {
+    this.setState({
+      input: event.target.value,
+    });
+  };
+
+  handleSearch = (event) => {
+    this.props.dispatch({
+      type: "SEARCH_USER",
+      payload: this.state.input,
+    });
+  };
+  handleBlur = (event) => {
+    console.log(event);
+    const {
+      name,
+      title,
+      minLength,
+      maxLength,
+      validationMessage,
+      validity: { valueMissing, tooShort, patternMismatch },
+    } = event.target;
+    // const {valueMissing} = validity
+    console.log(validationMessage);
+    console.log(patternMismatch);
+    // console.log(validity)
+
+    let mess = "";
+    if (valueMissing) {
+      mess = `${title} không được bỏ trống !`;
+    }
+
+    if (tooShort) {
+      mess = `${title} từ ${minLength} đến ${maxLength} ký tự !`;
+    }
+
+    if (patternMismatch) {
+      switch (name) {
+        case "userName":
+          mess = `${title} phải là số !`;
+          break;
+        case "fullName":
+          mess = `${title} phải là ký tự !`;
+          break;
+        case "phoneNumber":
+          mess = `${title} phải là 10 số !`;
+          break;
+        default:
+          mess = `${title} không đúng định dạng !`;
+      }
+    }
+
+    this.setState({
+      errors: {
+        ...this.state.errors,
+        [name]: mess,
+      },
+    });
+  };
+
+  // chuyển props thành state nội bộ
+  static getDerivedStateFromProps = (nextProps, currentState) => {
+    // console.log(nextProps, currentState)
+    if (
+      nextProps.selectedUser &&
+      nextProps.selectedUser.id !== currentState.values.id
+    ) {
+      currentState.values = nextProps.selectedUser;
+    }
+
+    return currentState;
+  };
+
+  render() {
+    const { userName, fullName, phoneNumber, email } = this.state.values;
+
+    return (
+      <form
+        // tắt pop-up khi handle
+        noValidate
+        // onSubmit={(event) => {
+        //     event.preventDefault()
+        //     console.log(this.state)
+        // }}
+        onSubmit={this.handleSubmit}
+      >
+        <div className="text-3xl text-white bg-gray-700 p-5">
+          Thông tin sinh viên
+        </div>
+        <div className="grid grid-cols-2 gap-6 mt-5">
+          <div>
+            <p>Mã sinh viên</p>
+            <input
+              required
+              pattern="^(0|[1-9][0-9]*)$"
+              minLength={4}
+              maxLength={15}
+              title="Mã sinh viên"
+              type="text"
+              value={userName}
+              name="userName"
+              placeholder="Mã sinh viên"
+              className="border-2 border-gray rounded-sm p-3 w-full mt-2"
+              onChange={this.handleState}
+              onBlur={this.handleBlur}
+            />
+            <span className="text-red-500 text-14">
+              {this.state.errors.userName}
+            </span>
+          </div>
+          <div>
+            <p>Họ tên</p>
+            <input
+              required
+              pattern="^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s\W|_]+"
+              title="Họ tên"
+              type="text"
+              value={fullName}
+              name="fullName"
+              placeholder="Họ tên"
+              className="border-2 border-gray rounded-sm p-3 w-full mt-2"
+              onChange={this.handleState}
+              onBlur={this.handleBlur}
+            />
+            <span className="text-red-500 text-14">
+              {this.state.errors.fullName}
+            </span>
+          </div>
+          <div>
+            <p>Số điện thoại</p>
+            <input
+              required
+              pattern="^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4}$"
+              title="Số điện thoại"
+              type="text"
+              value={phoneNumber}
+              name="phoneNumber"
+              placeholder="Số điện thoại"
+              className="border-2 border-gray rounded-sm p-3 w-full mt-2"
+              onChange={this.handleState}
+              onBlur={this.handleBlur}
+            />
+            <span className="text-red-500 text-14">
+              {this.state.errors.phoneNumber}
+            </span>
+          </div>
+          <div>
+            <p>Email</p>
+            <input
+              required
+              pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$"
+              title="Email"
+              type="text"
+              value={email}
+              name="email"
+              placeholder="Email"
+              className="border-2 border-gray rounded-sm p-3 w-full mt-2"
+              onChange={this.handleState}
+              onBlur={this.handleBlur}
+            />
+            <span className="text-red-500 text-14">
+              {this.state.errors.email}
+            </span>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className={`p-2 rounded-sm mr-4 bg-green-500 text-white cursor-pointer hover:bg-red-700 ${
+                !this.props.selectedUser ? "" : "hidden"
+              }`}
+            >
+              Thêm sinh viên
+            </button>
+            <button
+              type="submit"
+              className={`p-2 rounded-sm mr-4 bg-blue-500 text-white cursor-pointer hover:bg-yellow-700 ${
+                this.props.selectedUser ?? "hidden"
+              }`}
+            >
+              Cập nhật sinh viên
+            </button>
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Tìm kiếm sinh viên"
+              name="search"
+              value={this.state.input}
+              className="border-2 border-gray mr-10 rounded-sm p-3 w-60"
+              onChange={this.handleInput}
+            />
+
+            <button
+              className="p-2 rounded-sm mr-4 bg-blue-500 text-white cursor-pointer hover:bg-red-700"
+              onClick={this.handleSearch}
+            >
+              Tìm kiếm sinh viên
+            </button>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    ...state.baiTapForm,
+  };
+};
+
+export default connect(mapStateToProps)(FormDangKy);
---
+++ src/store/reducers/baiTapForm.js
@@ -0,0 +1,70 @@
+const stateDefault = {
+  mangSinhVien: [
+    {
+      id: 1234,
+      userName: 1111,
+      fullName: "tien",
+      phoneNumber: 123456789,
+      email: "tien@gmail.com",
+    },
+    {
+      id: 4567,
+      userName: 2222,
+      fullName: "van",
+      phoneNumber: 123456789,
+      email: "van@gmail.com",
+    },
+    {
+      id: 1122,
+      userName: 3333,
+      fullName: "cong",
+      phoneNumber: 123456789,
+      email: "cong@gmail.com",
+    },
+    {
+      id: 3344,
+      userName: 4444,
+      fullName: "thanh",
+      phoneNumber: 123456789,
+      email: "thanh@gmail.com",
+    },
+  ],
+  selectedUser: null,
+};
+
+export const baiTapForm = (state = stateDefault, { type, payload }) => {
+  switch (type) {
+    case "ADD_USER": {
+      const data = [...state.mangSinhVien];
+      const user = { ...payload, id: Date.now() };
+      data.push(user);
+      // console.log(user)
+      return { ...state, mangSinhVien: data };
+    }
+    case "DELETE_USER": {
+      const data = state.mangSinhVien.filter((item) => item.id !== payload);
+      return { ...state, mangSinhVien: data };
+    }
+    case "EDIT_USER": {
+      const user = state.mangSinhVien.find((item) => item.id === payload);
+      return { ...state, selectedUser: user };
+    }
+    case "UPDATE_USER": {
+      const newUser = state.mangSinhVien.map((item) =>
+        item.id === payload.id ? payload : item
+      );
+      state.selectedUser = null;
+
+      return { ...state, mangSinhVien: newUser };
+    }
+    case "SEARCH_USER": {
+      const searchUser = state.mangSinhVien.filter(
+        (item) => item.fullName === payload
+      );
+      return { ...state, mangSinhVien: searchUser };
+    }
+
+    default:
+      return state;
+  }
+};
